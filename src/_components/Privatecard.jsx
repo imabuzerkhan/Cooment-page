a6290791fd382d1b65b6e6f0b6eb5396facb9e17
imabuzerkhan <abuzerk41@gmail.com>
2024-11-28 17:35:34 +0545

    new commit

--- src/_components/Privatecard.jsx
+++ src/_components/Privatecard.jsx
@@ -1,4 +1,5 @@
-import { MessageCircleHeartIcon } from 'lucide-react'
+import { MdOutlineReplyAll } from "react-icons/md";
+
 import React from 'react'
 import { Button } from '@/components/ui/button'
 
@@ -8,7 +9,7 @@ const Privatecard = () => {
      <div className="public bg-green-50 w-[31rem] px-6 py-3  ">
       <h3 className='text-[1rem] font-serif' > If you’re in a serious crisis or feeling unsafe, please contact a trusted adult immediately or reach out to a mental health hotline in your area. Your well-being is important, and there are people who want to help you. ❤️</h3>
       <div className='flex justify-end my-1 ' >
-      <Button>Comment</Button>
+      <Button> <MdOutlineReplyAll /> Reply</Button>
       </div>
 
     </div>
--- src/_components/PublicCard.jsx
+++ src/_components/PublicCard.jsx
@@ -1,4 +1,4 @@
-
+import { MdOutlineComment } from "react-icons/md";
 import React from 'react'
 import { Button } from '@/components/ui/button'
 const PublicCard = () => {
@@ -7,7 +7,7 @@ const PublicCard = () => {
     <div className="public bg-green-50 w-[31rem] px-6 py-3  ">
       <h3 className='text-[1rem] font-serif' > If your school has a counselor or psychologist, they can provide the guidance and support you need. Alternatively, community mental health resources can be a great option.</h3>
       <div className='flex justify-end my-1 ' >
-      <Button>Reply</Button>
+      <Button> <MdOutlineComment /> Comment</Button>
       </div>
     </div>
     
